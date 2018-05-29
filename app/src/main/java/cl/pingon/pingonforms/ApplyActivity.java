245a6d0147bcbab6872a2dc22fa1ba2a56f52fc6
nicoundurraga <nicolas@pingon.cl>
2018-05-28 20:51:01 -0400

    commit

--- app/src/main/java/cl/pingon/pingonforms/ApplyActivity.java
+++ app/src/main/java/cl/pingon/pingonforms/ApplyActivity.java
@@ -1,7 +1,16 @@
 package cl.pingon.pingonforms;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ApplyActivity extends AppCompatActivity {
 
@@ -9,6 +18,14 @@ public class ApplyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply);
+
+        final EditText nameEt = findViewById(R.id.nameEt);
+        final EditText phoneEt = findViewById(R.id.phoneEt);
+        final EditText mailEt = findViewById(R.id.mailEt);
+        final RadioGroup applyareaRg = findViewById(R.id.interestareaRg);
+        final Button sendBt = findViewById(R.id.sendBt);
+
+
     }
 
 
